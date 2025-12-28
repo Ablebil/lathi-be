6bc27cdc711919f63067377cae53aea7a37c2a0f
Ablebil <hafizhfaiq07@gmail.com>
2025-12-29 00:27:45 +0700

    fix(middleware): make MiddlewareItf interface public

--- internal/middleware/mdidleware.go
+++ internal/middleware/mdidleware.go
@@ -1,9 +1,16 @@
 package middleware
 
-import "github.com/Ablebil/lathi-be/pkg/jwt"
+import (
+	"github.com/Ablebil/lathi-be/pkg/jwt"
+	"github.com/gofiber/fiber/v2"
+)
 
 type middleware struct {
 	jwt jwt.JwtItf
+}
+
+type MiddlewareItf interface {
+	Authenticate(ctx *fiber.Ctx) error
 }
 
 func NewMiddleware(jwt jwt.JwtItf) *middleware {
