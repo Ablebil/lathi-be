060a1383ea608dd5b680f3f75d418cb5aa3b6695
Ablebil <hafizhfaiq07@gmail.com>
2025-12-29 23:01:11 +0700

    feat(infra): add minio wrapper and storage config

---
+++ internal/infra/minio/minio.go
@@ -0,0 +1,41 @@
+package minio
+
+import (
+	"fmt"
+
+	"github.com/Ablebil/lathi-be/internal/config"
+	m "github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+type MinioItf interface {
+	GetObjectURL(object string) string
+}
+
+type minio struct {
+	client        *m.Client
+	endpoint      string
+	publicBaseURL string
+	bucket        string
+}
+
+func New(env *config.Env) (MinioItf, error) {
+	client, err := m.New(env.StorageEndpoint, &m.Options{
+		Creds:  credentials.NewStaticV4(env.StorageAccessKey, env.StorageSecretKey, ""),
+		Secure: true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &minio{
+		client:        client,
+		endpoint:      env.StorageEndpoint,
+		publicBaseURL: env.StoragePublicUrl,
+		bucket:        env.StorageBucket,
+	}, nil
+}
+
+func (m *minio) GetObjectURL(object string) string {
+	return fmt.Sprintf("https://%s/%s/%s", m.publicBaseURL, m.bucket, object)
+}
