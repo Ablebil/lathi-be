36fe527755235ce64bcbd8512ad5e312b9769d5c
Ablebil <hafizhfaiq07@gmail.com>
2026-01-05 23:40:55 +0700

    refactor(database): implement environment-based logging levels

--- internal/infra/postgresql/postgresql.go
+++ internal/infra/postgresql/postgresql.go
@@ -14,16 +14,23 @@ func New(env *config.Env) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
 		env.DBHost, env.DBUser, env.DBPassword, env.DBName, env.DBPort, env.DBSSLMode)
 
+	logLevel := logger.Silent
+	if env.AppEnv == "development" {
+		logLevel = logger.Info
+	}
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		PrepareStmt: true,
-		Logger:      logger.Default.LogMode(logger.Info),
+		Logger:      logger.Default.LogMode(logLevel),
 	})
-
-	db.Debug()
 
 	if err != nil {
 		slog.Error("failed to connect to postgres", "error", err)
 		return nil, err
+	}
+
+	if env.AppEnv == "development" {
+		db = db.Debug()
 	}
 
 	return db, nil
